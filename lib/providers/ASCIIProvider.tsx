743e3d566fbf4db8885fa9929ba9a2ba98032636
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-02-11 17:07:59 -0600

    Added options props

--- lib/providers/ASCIIProvider.tsx
+++ lib/providers/ASCIIProvider.tsx
@@ -1,37 +1,49 @@
+import { useMemo } from "react"
 import { GridContext } from "../contexts/GridContext"
 import { useWindowDimensions } from "../hooks/useWindowDimensions"
+import type { ASCIIProviderProps } from "../types/ASCIIProviderProps"
 import type { GridData } from "../types/GridData"
+import type { GridOptions } from "../types/GridOptions"
 import { defaultOptions } from "../utils/defaultOptions"
 
-function initGrid({ width, height }: { width: number; height: number }): GridData {
+function initGrid({ width, height, ...options }: { width: number; height: number } & Partial<GridOptions>): GridData {
+	const mergedOptions: GridOptions = {
+		...defaultOptions,
+		...options,
+	}
 	const fontHeight = 16
 	const courierRatio = 1229 / 2048
 	const fontWidth = fontHeight * courierRatio
 	const truncWidth = width - (width % fontWidth)
 	const truncHeight = height - (height % fontHeight)
-	const windowWidth = width
-	const windowHeight = height
 	const rows = Math.floor(truncHeight / fontHeight)
 	const cols = Math.floor(truncWidth / fontWidth)
-	const grid = Array.from({ length: rows * cols }, () => String.fromCharCode(160))
-	const options = defaultOptions
+	const grid = Array.from({ length: rows * cols }, () => mergedOptions.fill)
 	return {
 		fontHeight,
 		courierRatio,
 		fontWidth,
 		truncWidth,
 		truncHeight,
-		windowWidth,
-		windowHeight,
+		windowWidth: width,
+		windowHeight: height,
 		rows,
 		cols,
 		grid,
-		options,
+		options: mergedOptions,
 	}
 }
 
-export function ASCIIProvider({ children }: { children: React.ReactNode }) {
+export function ASCIIProvider({ children, ...options }: ASCIIProviderProps) {
+	// const parentGrid = useContext(GridContext)
+
+	// // if a grid already exists, reuse it
+	// if (parentGrid) {
+	// 	return <GridContext.Provider value={parentGrid}>{children}</GridContext.Provider>
+	// }
+
 	const { width, height } = useWindowDimensions()
-	const grid = initGrid({ width, height })
+	const grid = useMemo(() => initGrid({ width, height, ...options }), [width, height, options])
+	//const grid = initGrid({ width, height, ...options })
 	return <GridContext.Provider value={grid}>{children}</GridContext.Provider>
 }
---
+++ lib/types/ASCIIProviderProps.tsx
@@ -0,0 +1,3 @@
+import type { GridOptions } from "./GridOptions"
+
+export type ASCIIProviderProps = React.PropsWithChildren<Partial<GridOptions>>
