ed3508b600e131fb6aaf229ca0fc9c9468e70f2a
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-02-19 23:30:23 -0600

    update

--- TODO.md
+++ TODO.md
@@ -8,4 +8,7 @@
 - [ ] Add `<ul>` and `<ol>` bullet points and numbers
 - [ ] Add section about grid reveal to README.md
 - [ ] Add demo page
+- [x] Set reveal speed in relation to grid size
+- [x] Refresh and reveal grid on resize
+- [x] Adding a third corner to an intersection breaks the intersection, add rules to preserve the intersection character if the intersection character is already there
 - [x] Add regular image functionality #feature images-general #release 0.4.0
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-//import Draggable from "react-draggable"
+import Draggable from "react-draggable"
 import { ASCII } from "../lib/components/ASCII"
 import { useRef } from "react"
+import { useGridContext } from "../lib/hooks/useGridContext"
 
 function App() {
 	const nodeRef = useRef(null)
-	//const grid = useGridContext()
+	const grid = useGridContext()
 	return (
 		<ASCII>
-			<div className="flex justify-center">
-				{/* <Draggable nodeRef={nodeRef} grid={[grid.fontWidth, grid.fontHeight]}> */}
+			{/* <div className="flex justify-center"> */}
+			<Draggable nodeRef={nodeRef} grid={[grid.fontWidth, grid.fontHeight]}>
 				<div
 					ref={nodeRef}
-					className="w-[500px] h-72 border text-center ascii-border ascii-text whitespace-pre pointer-events-auto cursor-grab"
+					className="w-[599px] h-71 border ascii-border ascii-text whitespace-pre pointer-events-auto cursor-grab"
 				>
 					{String.raw`
-   /$$                           /$$    
-  | $$                          | $$    
- /$$$$$$    /$$$$$$   /$$$$$$$ /$$$$$$  
-|_  $$_/   /$$__  $$ /$$_____/|_  $$_/  
-  | $$    | $$$$$$$$|  $$$$$$   | $$    
-  | $$ /$$| $$_____/ \____  $$  | $$ /$$
-  |  $$$$/|  $$$$$$$ /$$$$$$$/  |  $$$$/
-   \___/   \_______/|_______/    \___/  
-                                        
+   /$$
+  | $$
+ /$$$$$$    /$$$$$$
+|_  $$_/   /$$__  $$
+  | $$    | $$$$$$$$|
+  | $$ /$$| $$_____/
+  |  $$$$/|  $$$$$$$
+   \___/   \_______/          
 					`}
 					<a href="https://en.wikipedia.org/wiki/Block_Elements" className="ascii-border ascii-text underline">
 						a link
@@ -35,8 +35,62 @@ function App() {
 						a link
 					</a> */}
 				</div>
-				{/* </Draggable> */}
-				{/* <textarea className="w-64 h-64 ascii-border" defaultValue={"test"}></textarea>
+			</Draggable>
+			<Draggable nodeRef={nodeRef} grid={[grid.fontWidth, grid.fontHeight]}>
+				<div
+					ref={nodeRef}
+					className="w-[599px] h-71 border ascii-border ascii-text whitespace-pre pointer-events-auto cursor-grab"
+				>
+					{String.raw`
+   /$$
+  | $$
+ /$$$$$$    /$$$$$$
+|_  $$_/   /$$__  $$
+  | $$    | $$$$$$$$|
+  | $$ /$$| $$_____/
+  |  $$$$/|  $$$$$$$
+   \___/   \_______/          
+					`}
+					<a href="https://en.wikipedia.org/wiki/Block_Elements" className="ascii-border ascii-text underline">
+						a link
+					</a>
+					{/* <div className="m-4 ascii-border-tl ascii-border-br h-8" />
+					<div className="m-2 border h-8 ascii-border-r ascii-border-b" />
+					<button className="ascii-border ascii-text">button</button>
+					<br></br>
+					<a href="https://en.wikipedia.org/wiki/Block_Elements" className="ascii-border ascii-text">
+						a link
+					</a> */}
+				</div>
+			</Draggable>
+			<Draggable nodeRef={nodeRef} grid={[grid.fontWidth, grid.fontHeight]}>
+				<div
+					ref={nodeRef}
+					className="w-[599px] h-71 border ascii-border ascii-text whitespace-pre pointer-events-auto cursor-grab"
+				>
+					{String.raw`
+   /$$
+  | $$
+ /$$$$$$    /$$$$$$
+|_  $$_/   /$$__  $$
+  | $$    | $$$$$$$$|
+  | $$ /$$| $$_____/
+  |  $$$$/|  $$$$$$$
+   \___/   \_______/          
+					`}
+					<a href="https://en.wikipedia.org/wiki/Block_Elements" className="ascii-border ascii-text underline">
+						a link
+					</a>
+					{/* <div className="m-4 ascii-border-tl ascii-border-br h-8" />
+					<div className="m-2 border h-8 ascii-border-r ascii-border-b" />
+					<button className="ascii-border ascii-text">button</button>
+					<br></br>
+					<a href="https://en.wikipedia.org/wiki/Block_Elements" className="ascii-border ascii-text">
+						a link
+					</a> */}
+				</div>
+			</Draggable>
+			{/* <textarea className="w-64 h-64 ascii-border" defaultValue={"test"}></textarea>
 				<br></br>
 				<div className="p-4">
 					{" "}
@@ -50,10 +104,10 @@ function App() {
 						<option value="audi">Audi</option>
 					</select>
 				</div> */}
-				{/* <div className="ascii p-4">
+			{/* <div className="ascii p-4">
 					<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmancelona-vet.com%2Fwp-content%2Fuploads%2F2022%2F04%2FPuppy-Care.png&f=1&nofb=1&ipt=fdd8b957d76c0c6eda753c0b89ca41de8642c16b654126bf5cd82e61c0d114ea"></img>
 				</div> */}
-			</div>
+			{/* </div> */}
 		</ASCII>
 	)
 }
