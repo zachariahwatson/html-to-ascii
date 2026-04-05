042706deb727390e7ee32a1485c64f9864e15565
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-04-05 04:00:59 -0500

    Merge options before initGrid to make fontPath optional

--- lib/providers/ASCIIProvider.tsx
+++ lib/providers/ASCIIProvider.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from "react"
 import { GridContext } from "../contexts/GridContext"
 import { useWindowDimensions } from "../hooks/useWindowDimensions"
-import type { ASCIIProviderProps } from "../types/ASCIIProviderProps"
 import type { GridData } from "../types/GridData"
 import type { GridOptions } from "../types/GridOptions"
 import { defaultOptions } from "../utils/defaultOptions"
@@ -11,15 +10,10 @@ function initGrid({
 	height,
 	fontData,
 	...options
-}: { width: number; height: number; fontData: Font } & Partial<GridOptions>): GridData {
-	const mergedOptions: GridOptions = {
-		...defaultOptions,
-		...options,
-	}
-
+}: { width: number; height: number; fontData: Font } & GridOptions): GridData {
 	//calculate font dimensions
 	const unitsPerEm = fontData.unitsPerEm || 1000
-	const fontSize = mergedOptions.fontSize
+	const fontSize = options.fontSize
 	const fontWidth = (fontData.tables.hhea.advanceWidthMax * fontSize) / unitsPerEm
 	const fontHeight = ((fontData.tables.hhea.ascender - fontData.tables.hhea.descender) * fontSize) / unitsPerEm
 
@@ -28,7 +22,7 @@ function initGrid({
 	const truncHeight = height - (height % fontHeight)
 	const rows = Math.floor(truncHeight / fontHeight)
 	const cols = Math.floor(truncWidth / fontWidth)
-	const grid = Array.from({ length: rows * cols }, () => mergedOptions.fill)
+	const grid = Array.from({ length: rows * cols }, () => options.fill)
 
 	return {
 		fontHeight,
@@ -40,26 +34,31 @@ function initGrid({
 		rows,
 		cols,
 		grid,
-		options: mergedOptions,
+		options: options,
 	}
 }
 
-export function ASCIIProvider({ children, ...options }: ASCIIProviderProps) {
+export function ASCIIProvider({ children, ...options }: React.PropsWithChildren<Partial<GridOptions>>) {
 	const { width, height } = useWindowDimensions()
 	const [fontData, setFontData] = useState<Font | null>(null)
 	//const defaultFontPath = new URL(CascadiaMono, import.meta.url)
 
+	const mergedOptions: GridOptions = {
+		...defaultOptions,
+		...options,
+	}
+
 	useEffect(() => {
-		fetch(options.fontPath)
+		fetch(mergedOptions.fontPath)
 			.then((res) => res.arrayBuffer())
 			.then((data) => {
 				setFontData(parse(data))
 			})
-	}, [options.fontPath])
+	}, [mergedOptions.fontPath])
 
 	const grid = useMemo(() => {
 		if (!fontData) return null
-		return initGrid({ width, height, fontData, ...options })
+		return initGrid({ width, height, fontData, ...mergedOptions })
 	}, [width, height, fontData, options])
 
 	if (!grid) return null
